c9d8294864682c2d99513a6b224c9f19f09c6739
a-g-sanchez <aarongsanchez@google.com>
2023-10-10 15:05:19 -0500

    Completed Song tests

--- index.test.js
+++ index.test.js
@@ -23,6 +23,11 @@ describe('Band, Musician, and Song Models', () => {
         expect(newMusician).toBeInstanceOf(Musician);
     })
 
+    test('can create a Song', async () => {
+        let newSong = await Song.create({title: 'All Star', year: 1978, length: 1.56})
+        expect(newSong).toBeInstanceOf(Song)
+    })
+
     test('can update a Band', async () => {
         // TODO - test updating a band
         let band1 = await Band.create({name:'Maroon5', genre:'Rock'});
@@ -34,6 +39,12 @@ describe('Band, Musician, and Song Models', () => {
         let musician1 = await Musician.create({name: 'Kendrick', instrument: 'voice'});
         await musician1.update({instrument: 'drums'})
         expect(musician1.instrument).toBe('drums');
+    })
+
+    test('can update a Song', async () => {
+        let song = await Song.findByPk(1)
+        await song.update({year: 1999})
+        expect(song.year).toBe(1999)
     })
 
     test('can delete a Band', async () => {
@@ -49,5 +60,12 @@ describe('Band, Musician, and Song Models', () => {
         let deletedMus = await Musician.destroy({where: {id:1}});
         let allMusicians = await Musician.findAll();
         expect(allMusicians.length).toBe(1);
+    })
+
+    test('can delete a Song', async () => {
+        let song = await Song.findByPk(1)
+        await song.destroy()
+        let allSongs = await Song.findAll()
+        expect(allSongs.length).toBe(0)
     })
 })
