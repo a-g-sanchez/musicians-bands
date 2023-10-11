82554eabab5989ba2f014ec3c6c82bc906ee7b9e
Tina Sun <suntina@google.com>
2023-10-11 15:09:52 -0400

    band musician test

--- models/Musician.js
+++ models/Musician.js
@@ -1,7 +1,7 @@
 const {Sequelize, db, DataTypes} = require('../db');
 
 // TODO - define the Musician model
-const Musician = db.define('Muscian',{
+const Musician = db.define('Musician',{
     name: DataTypes.STRING,
     instrument: DataTypes.STRING
 });
